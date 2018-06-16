30abf8277c51de2fac7a479c9c086cf6bb863144
Sumit Chaudhary <digitalcodecorporation@gmail.com>
2018-06-16 19:24:15 +0530

    Wilcard Routing & redirectTo

--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes = [
   {path: '' , component:HomeComponent},
@@ -21,7 +22,10 @@ const appRoutes = [
   {path: 'servers',component:ServersComponent,children:[
     {path: ':id',component:ServerComponent},
     {path: ':id/edit',component:EditServerComponent}
-  ]}
+  ]},
+  {path:'something',component:NotFoundComponent},
+  // {path:'not-found',redirectTo:'something'}
+  {path:'**',redirectTo:'something'}
 ]
 
 @NgModule({
@@ -32,7 +36,8 @@ const appRoutes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
